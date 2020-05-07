933666fde1c1174ead2ebcd83a8e748140b4befe
Bohdan Donchenko <bohdan.donchenko@sap.com>
2020-05-07 18:52:25 +0300

    first working k8s version

--- src/app/shared/ownid/ownid.component.ts
+++ src/app/shared/ownid/ownid.component.ts
@@ -30,7 +30,8 @@ export class OwnidComponent implements OnInit {
       type: this.type,
       onLogin: this.onLogin.emit.bind(this.onLogin),
       onRegister: this.onRegister.emit.bind(this.onRegister),
-      statusInterval: 3000
+      statusInterval: 3000,
+      getContextURL: "/netcore3/ownid"
     });
   }
 }
