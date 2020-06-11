da8fe31afeee90d69fcbee09046a3ead67adb91f
Bohdan Donchenko <bohdan.donchenko@sap.com>
2020-06-11 14:35:03 +0300

    added demo app deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,23 @@
 #!bin/sh
 
 REPOSITORY_URI=$1
-IMAGE_TAG=$2
-ENV=$3
+REPOSITORY_DEMO_URI=$2
+IMAGE_TAG=$3
+ENV=$4
 
 echo Pushing image $REPOSITORY_URI:$IMAGE_TAG to registry
 docker tag ownid-client-app:latest $REPOSITORY_URI:$IMAGE_TAG
 docker push $REPOSITORY_URI:$IMAGE_TAG
 
-echo Updating image in Cluster deployment
+echo Pushing image $REPOSITORY_DEMO_URI:$IMAGE_TAG to registry
+docker tag ownid-demo-app:latest $REPOSITORY_DEMO_URI:$IMAGE_TAG
+docker push $REPOSITORY_DEMO_URI:$IMAGE_TAG
+
 kubectl apply -f manifests/$ENV.yaml
+
+echo Updating Client-app image in Cluster deployment
 kubectl -n=$ENV set image deployment/ownid-client-app-deployment ownid-client-app=$REPOSITORY_URI:$IMAGE_TAG --record
 kubectl -n=$ENV set image deployment/ownid-client-app-2-deployment ownid-client-app-2=$REPOSITORY_URI:$IMAGE_TAG --record
+
+echo Updating Demo-app image in Cluster deployment
+kubectl -n=$ENV set image deployment/ownid-demo-app-deployment ownid-demo-app=$REPOSITORY_DEMO_URI:$IMAGE_TAG --record
