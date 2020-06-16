568d03a01ff8fe4e106d4af952d4861f64a3cd8a
Anton Pashchenko <anton.pashchenko@sap.com>
2020-06-16 11:44:51 +0300

    added logout to client app

--- src/app/pages/user/user-page.component.ts
+++ src/app/pages/user/user-page.component.ts
@@ -1,8 +1,8 @@
-import {ChangeDetectionStrategy, Component, NgZone} from "@angular/core";
-import {BehaviorSubject} from "rxjs";
-import {AppStore, IProfile} from "../../app.store";
-import {SetProfileCommand} from "../sign/commands/set-profile.command";
-import {GigyaService} from "@services/gigya.service";
+import { ChangeDetectionStrategy, Component, NgZone } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { GigyaService } from "@services/gigya.service";
+import { AppStore, IProfile } from "../../app.store";
+import { SetProfileCommand } from "../sign/commands/set-profile.command";
 
 @Component({
   selector: "app-user-page",
@@ -12,21 +12,28 @@ import {GigyaService} from "@services/gigya.service";
 })
 export class UserPageComponent {
   profile$: BehaviorSubject<IProfile>;
+
   linked$: BehaviorSubject<boolean>;
 
-  constructor(private store: AppStore, 
+  constructor(private store: AppStore,
               private setProfileCommand: SetProfileCommand,
               private gigyaService: GigyaService,
-              private ngZone: NgZone) {
+              private ngZone: NgZone,
+  ) {
     this.profile$ = this.store.profile$;
-    this.linked$ =  new BehaviorSubject<boolean>(false);
-    
-    this.gigyaService.getProfile((data: any)=>{
+    this.linked$ = new BehaviorSubject<boolean>(false);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    this.gigyaService.getProfile((data: any) => {
       this.setProfileCommand.execute(data.profile);
     });
   }
-  
+
   onLink() {
     this.ngZone.run(() => this.linked$.next(true));
+  }
+
+  onLogout() {
+    this.gigyaService.logout();
   }
 }
--- src/app/pages/user/user-page.module.ts
+++ src/app/pages/user/user-page.module.ts
@@ -5,10 +5,11 @@ import { UserPageComponent } from "./user-page.component";
 import { UserPageRoutingModule } from "./user-routing.module";
 import {SetProfileCommand} from "../sign/commands/set-profile.command";
 import {OwnidModule} from "../../shared/ownid/ownid.module";
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [UserPageComponent],
-  imports: [CommonModule, UserPageRoutingModule, MatCardModule, OwnidModule],
+  imports: [CommonModule, UserPageRoutingModule, MatCardModule, OwnidModule, MatButtonModule],
   providers: [SetProfileCommand]
 })
 export class UserPageModule {}
