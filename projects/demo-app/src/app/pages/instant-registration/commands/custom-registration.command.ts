a2dde025e0256fb8d3c6a1261d66865e24f5c997
Anton Pashchenko <anton.pashchenko@sap.com>
2020-07-31 13:53:21 +0300

    #78 gigya registation on auth only flow (#104)

--- projects/demo-app/src/app/pages/instant-registration/commands/custom-registration.command.ts
+++ projects/demo-app/src/app/pages/instant-registration/commands/custom-registration.command.ts
@@ -1,55 +1,64 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 
 import { GigyaService } from '@services/gigya.service';
 
 import { AppStore } from '../../../app.store';
 import { IDataCommand } from '../../i-data-command';
-import { environment } from '../../../../environments/environment';
 
-const REGISTER_URL = environment.ownidURLPrefix.replace('ownid', 'not-ownid/register');
+interface GigyaRequestData {
+  email: string;
+  firstName: string;
+  password: string;
+  data?: {
+    ownIdConnections: {
+      keyHsh: string;
+      pubKey: string;
+    }[]
+  }
+}
+
 
 @Injectable()
 export class CustomRegistrationCommand implements IDataCommand<{ data: { [key: string]: string }, ownidWidget: unknown }> {
   constructor(
-    private router: Router,
     private appStore: AppStore,
-    private httpClient: HttpClient,
     private gigyaService: GigyaService,
   ) {}
 
   async execute({ data, ownidWidget }: { data: { [key: string]: string }, ownidWidget: unknown }) {
     this.appStore.formError$.next(null);
 
-    if (!data.skipPassword) {
-      const { email, firstName, password } = data;
-      this.gigyaService.register({ email, firstName, password }, (resp: { status: string, errorDetails: string }) => {
-        if (resp.status === 'FAIL') {
-          this.appStore.formError$.next(resp.errorDetails);
-        }
-      });
-      return;
+    const { email, firstName, password } = data;
+
+    let gigyaRequestData: GigyaRequestData = { email, firstName, password };
+
+    if (data.skipPassword) {
+      // @ts-ignore
+      const ownidResponse = await window.ownid.getOwnIDPayload(ownidWidget);
+
+      if (ownidResponse.error) {
+        this.appStore.formError$.next(ownidResponse.message);
+        return;
+      }
+
+      gigyaRequestData = {
+        email,
+        firstName,
+        // @ts-ignore
+        password: window.ownid.generateOwnIDPassword(12),
+        data: {
+          ownIdConnections: [{
+            keyHsh: ownidResponse.data.hash,
+            pubKey: ownidResponse.data.publicKey,
+          }]
+        },
+      };
     }
 
-    // @ts-ignore
-    const ownidResponse = await window.ownid.getOwnIDPayload(ownidWidget);
-
-    if (ownidResponse.error) {
-      this.appStore.formError$.next(ownidResponse.message);
-      return;
-    }
-
-    this.httpClient.post(REGISTER_URL, { ...data, pubKey: ownidResponse.data.publicKey })
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      .subscribe((statusRS: any) => {
-        document.cookie = `${ statusRS.sessionInfo.cookieName }=${ statusRS.sessionInfo.cookieValue }; path=/`;
-
-        this.router.navigateByUrl(`/notes`);
-
-      }, (errorObject: { error: { error: string } }) => {
-        this.appStore.formError$.next(errorObject.error.error);
-      });
-
+    this.gigyaService.register(gigyaRequestData, (resp: { status: string, errorDetails: string }) => {
+      if (resp.status === 'FAIL') {
+        this.appStore.formError$.next(resp.errorDetails);
+      }
+    });
   }
 }
