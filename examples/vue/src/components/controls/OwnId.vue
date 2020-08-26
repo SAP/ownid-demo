dde3890e3c42ff796f6042dcdde693d742bc2498
Avital Ben Natan <avital.ben-natan@sap.com>
2020-08-26 16:21:11 +0300

    Added Vue example application

---
+++ examples/vue/src/components/controls/OwnId.vue
@@ -0,0 +1,55 @@
+<template>
+  <div ref="ownid-container" class="ownid-container" />
+</template>
+
+<script>
+/* eslint-disable no-unused-vars */
+
+const environment = {
+  ownidURLPrefix : process.env.VUE_APP_OWNID_URL_PREFIX,
+  gigyaApiKey: process.env.VUE_APP_GIGYA_API_KEY
+};
+
+export default {
+  props: ['type', 'data'],
+  async mounted() {
+    var vm = this;
+
+    // @ts-ignore-next-line
+    window.ownid.init({
+      statusInterval: 1000,
+      URLPrefix: environment.ownidURLPrefix
+      // logger: new ConsoleLogger(),
+      // logLevel: 'info'
+    })
+
+    console.log(this.type)
+    if (this.type === 'link') {
+      // @ts-ignore-next-line
+      this.ownidWidget = await window.ownid.renderLinkGigya(
+        {
+          element: vm.$refs['ownid-container'],
+          type: this.type,
+          onLink: () => vm.$emit('link'),
+        },
+        environment.gigyaApiKey
+      )
+    } else {
+      // @ts-ignore-next-line
+      this.ownidWidget = window.ownid.render({
+        element: vm.$el,
+        type: this.type,
+        data: this.data,
+        onLogin: vm.$emit.bind(vm, 'login'),
+        onRegister: vm.$emit.bind(vm, 'register'),
+        onLink: vm.$emit.bind(vm,'link'),
+        onRecover: vm.$emit.bind(vm, 'recover')
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
