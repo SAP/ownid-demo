d0b334eb74f6f7eab65d3cd2620d95fad7bd3e2c
Bohdan Donchenko <bohdan.donchenko@sap.com>
2020-10-09 20:03:01 +0300

    fix demo2

--- projects/demo-app/src/environments/environment.dev2.ts
+++ projects/demo-app/src/environments/environment.dev2.ts
@@ -4,5 +4,5 @@ export const environment: IEnvironment = {
   production: true,
   gigyaApiKey: '3_hOdIVleWrXNvjArcZRwHJLiGA4e6Jrcwq7RfH5nL7ZUHyI_77z43_IQrJYxLbiq_',
   ownidURLPrefix: 'https://passwordless.demo.dev.skipthepassword.com/ownid',
-  flow: OwnIDFlow.authOnly,
+  flow: OwnIDFlow.classic,
 };
