c5d53db9b2470af1bf2d405fe9922bf3903fb5ff
Bohdan Donchenko <bohdan.donchenko@sap.com>
2020-10-09 18:14:27 +0300

    fix build demo2

--- scripts/build_demo.sh
+++ scripts/build_demo.sh
@@ -18,7 +18,7 @@ cd projects/demo-app && docker build -t ownid-demo-app:latest . && cd ../..
 # cat projects/demo-app/dist/index.html
 # cd projects/demo-app && docker build -t ownid-demo-app:latest2 . && cd ../..
 
-yarn ng build demo-app --configuration=$ENV2
+yarn ng build demo-app --configuration=${ENV}2
 sed -i -r -e "s/<!--gigya-->((.)*)<!--gigya-->/<script src=\"https:\/\/cdns.gigya.com\/js\/gigya.js?apikey=${GIGYA_DEMO}\"><\/script>/" projects/demo-app/dist/index.html
 sed -i -r -e "s/<!--ownid-->((.)*)<!--ownid-->/<script src=\"https:\/\/cdn.ownid.com\/${ENV}\/js\/latest\/gigya-sdk.es5.js\"><\/script>/" projects/demo-app/dist/index.html
 cat projects/demo-app/dist/index.html
