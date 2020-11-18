76ffd4dd925e4203940b82521177a6a2a3329275
Bohdan Donchenko <bohdan.donchenko@sap.com>
2020-11-18 16:22:58 +0200

    nginx cleanup

--- scripts/build_demo.sh
+++ scripts/build_demo.sh
@@ -2,7 +2,6 @@
 
 ENV=$1
 GIGYA_DEMO=$2
-GIGYA_DEMO_2=$3
 
 # demo app
 yarn ng build demo-app --configuration=$ENV
@@ -11,20 +10,11 @@ sed -i -r -e "s/<!--ownid-->((.)*)<!--ownid-->/<script src=\"https:\/\/cdn.ownid
 cat projects/demo-app/dist/index.html
 cd projects/demo-app && docker build -t ownid-demo-app:latest . && cd ../..
 
-# demo 2 app
-# yarn ng build demo-app --configuration=${ENV}2
-# sed -i -r -e "s/<!--gigya-->((.)*)<!--gigya-->/<script src=\"https:\/\/cdns.gigya.com\/js\/gigya.js?apikey=${GIGYA_DEMO_2}\"><\/script>/" projects/demo-app/dist/index.html
-# sed -i -r -e "s/<!--ownid-->((.)*)<!--ownid-->/<script src=\"https:\/\/cdn.ownid.com\/${ENV}\/js\/latest\/gigya-sdk.es5.js\"><\/script>/" projects/demo-app/dist/index.html
-# cat projects/demo-app/dist/index.html
-# cd projects/demo-app && docker build -t ownid-demo-app:latest2 . && cd ../..
-
 yarn ng build demo-app --configuration=${ENV}2
 sed -i -r -e "s/<!--gigya-->((.)*)<!--gigya-->/<script src=\"https:\/\/cdns.gigya.com\/js\/gigya.js?apikey=${GIGYA_DEMO}\"><\/script>/" projects/demo-app/dist/index.html
 sed -i -r -e "s/<!--ownid-->((.)*)<!--ownid-->/<script src=\"https:\/\/cdn.ownid.com\/${ENV}\/js\/latest\/gigya-sdk.es5.js\"><\/script>/" projects/demo-app/dist/index.html
 cat projects/demo-app/dist/index.html
 cd projects/demo-app && docker build -t ownid-demo-app:latest2 . && cd ../..
-
-
 
 yarn ng build demo-app --configuration=${ENV}3
 sed -i -r -e "s/<!--gigya-->((.)*)<!--gigya-->/<script src=\"https:\/\/cdns.gigya.com\/js\/gigya.js?apikey=${GIGYA_DEMO}\"><\/script>/" projects/demo-app/dist/index.html
