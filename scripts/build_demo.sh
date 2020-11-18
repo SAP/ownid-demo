9690725b0023d14059a422ba4241cf5a3555b904
Bohdan Donchenko <bohdan.donchenko@sap.com>
2020-11-18 16:58:13 +0200

    pwd

--- scripts/build_demo.sh
+++ scripts/build_demo.sh
@@ -11,6 +11,7 @@ cat projects/demo-app/dist/index.html
 cd projects/demo-app && docker build -t ownid-demo-app:latest . && cd ../..
 
 yarn ng build demo-app --configuration=${ENV}2
+pwd
 sed -i -r -e "s/<!--gigya-->((.)*)<!--gigya-->/<script src=\"https:\/\/cdns.gigya.com\/js\/gigya.js?apikey=${GIGYA_DEMO}\"><\/script>/" projects/demo-app/dist/index.html
 sed -i -r -e "s/<!--ownid-->((.)*)<!--ownid-->/<script src=\"https:\/\/cdn.ownid.com\/${ENV}\/js\/latest\/gigya-sdk.es5.js\"><\/script>/" projects/demo-app/dist/index.html
 cat projects/demo-app/dist/index.html
