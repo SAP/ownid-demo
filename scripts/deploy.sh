210213c6c1d775d0c7fc40a9df574b558982ff71
Bohdan Donchenko <bohdan.donchenko@sap.com>
2020-12-21 18:02:13 +0200

    added build number

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,10 @@
 #!bin/sh
 
 ENV=$1
+BUILD_NUMBER=$2
 
 # Demo update
-IMAGE_URI=$DOCKER_URL/$ENV/demo/ownid-demo_${TRAVIS_BUILD_NUMBER-}:$TRAVIS_COMMIT
+IMAGE_URI=$DOCKER_URL/$ENV/demo/ownid-demo:${BUILD_NUMBER-}
 echo Push demo $IMAGE_URI to registry
 docker tag ownid-demo-app:latest $IMAGE_URI
 docker push $IMAGE_URI
@@ -13,7 +14,7 @@ kubectl apply -f manifests/$ENV/demo.yaml
 kubectl -n=$ENV set image deployment/ownid-demo-app-deployment ownid-demo-app=$IMAGE_URI --record
 
 # Demo 2 update
-IMAGE_URI=$DOCKER_URL/$ENV/demo/ownid-demo-2_${TRAVIS_BUILD_NUMBER-}:$TRAVIS_COMMIT
+IMAGE_URI=$DOCKER_URL/$ENV/demo/ownid-demo-2:${BUILD_NUMBER-}
 echo Push demo2 $IMAGE_URI to registry
 docker tag ownid-demo-app:latest2 $IMAGE_URI
 docker push $IMAGE_URI
@@ -22,7 +23,7 @@ echo Updating DEMO2
 kubectl -n=$ENV set image deployment/ownid-demo-app-2-deployment ownid-demo-app-2=$IMAGE_URI --record
 
 # Demo 3 update
-IMAGE_URI=$DOCKER_URL/$ENV/demo/ownid-demo-3_${TRAVIS_BUILD_NUMBER-}:$TRAVIS_COMMIT
+IMAGE_URI=$DOCKER_URL/$ENV/demo/ownid-demo-3:${BUILD_NUMBER-}
 echo Push demo3 $IMAGE_URI to registry
 docker tag ownid-demo-app:latest3 $IMAGE_URI
 docker push $IMAGE_URI
@@ -32,7 +33,7 @@ kubectl -n=$ENV set image deployment/ownid-demo-app-3-deployment ownid-demo-app-
 
 
 # Demo 4 update
-IMAGE_URI=$DOCKER_URL/$ENV/demo/ownid-demo-4_${TRAVIS_BUILD_NUMBER-}:$TRAVIS_COMMIT
+IMAGE_URI=$DOCKER_URL/$ENV/demo/ownid-demo-4:${BUILD_NUMBER-}
 echo Push demo4 $IMAGE_URI to registry
 docker tag ownid-demo-app:latest4 $IMAGE_URI
 docker push $IMAGE_URI
