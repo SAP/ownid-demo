8a5389b2a9a7e9e62b9ce58651f3dcefbcb3ce8a
Bohdan Donchenko <bohdan.donchenko@sap.com>
2020-12-21 17:48:32 +0200

    reverted docker build

--- scripts/build_demo.sh
+++ scripts/build_demo.sh
@@ -6,20 +6,20 @@ GIGYA_DEMO=$2
 yarn ng build demo-app --configuration=$ENV
 sed -i -r -e "s/<!--gigya-->((.)*)<!--gigya-->/<script src=\"https:\/\/cdns.gigya.com\/js\/gigya.js?apikey=${GIGYA_DEMO}\"><\/script>/" projects/demo-app/dist/index.html
 sed -i -r -e "s/<!--ownid-->((.)*)<!--ownid-->/<script src=\"https:\/\/cdn.${ENV}.ownid.com\/js\/gigya-sdk.es5.js\"><\/script>/" projects/demo-app/dist/index.html
-docker build ./projects/demo-app -t ownid-demo-app:latest .
+cd projects/demo-app && docker build -t ownid-demo-app:latest . && cd ../..
 
 yarn ng build demo-app --configuration=${ENV}2
 sed -i -r -e "s/<!--gigya-->((.)*)<!--gigya-->/<script src=\"https:\/\/cdns.gigya.com\/js\/gigya.js?apikey=${GIGYA_DEMO}\"><\/script>/" projects/demo-app/dist/index.html
 sed -i -r -e "s/<!--ownid-->((.)*)<!--ownid-->/<script src=\"https:\/\/cdn.${ENV}.ownid.com\/js\/gigya-sdk.es5.js\"><\/script>/" projects/demo-app/dist/index.html
-docker build ./projects/demo-app -t ownid-demo-app:latest2 .
+cd projects/demo-app && docker build -t ownid-demo-app:latest2 . && cd ../..
 
 yarn ng build demo-app --configuration=${ENV}3
 sed -i -r -e "s/<!--gigya-->((.)*)<!--gigya-->/<script src=\"https:\/\/cdns.gigya.com\/js\/gigya.js?apikey=${GIGYA_DEMO}\"><\/script>/" projects/demo-app/dist/index.html
 sed -i -r -e "s/<!--ownid-->((.)*)<!--ownid-->/<script src=\"https:\/\/cdn.${ENV}.ownid.com\/js\/gigya-sdk.es5.js\"><\/script>/" projects/demo-app/dist/index.html
-docker build ./projects/demo-app -t ownid-demo-app:latest3 .
+cd projects/demo-app && docker build -t ownid-demo-app:latest3 . && cd ../..
 
 
 yarn ng build demo-app --configuration=${ENV}4
 sed -i -r -e "s/<!--gigya-->((.)*)<!--gigya-->/<script src=\"https:\/\/cdns.gigya.com\/js\/gigya.js?apikey=${GIGYA_DEMO}\"><\/script>/" projects/demo-app/dist/index.html
 sed -i -r -e "s/<!--ownid-->((.)*)<!--ownid-->/<script src=\"https:\/\/cdn.${ENV}.ownid.com\/js\/gigya-sdk.es5.js\"><\/script>/" projects/demo-app/dist/index.html
-docker build ./projects/demo-app -t ownid-demo-app:latest4 .
+cd projects/demo-app && docker build -t ownid-demo-app:latest4 . && cd ../..
